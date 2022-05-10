8cf2014952d19325e3b406d47d54be51d9f43f3a
dior011 <diyorraxmatullaev3@gmail.com>
2022-05-10 17:01:04 +0500

    baxa_dobriy

---
+++ script.js
@@ -0,0 +1,9 @@
+let zina = +prompt('zinalar sonini kiriting')
+let qator = prompt('qator belgisi !')
+let belgi = prompt ('oxirgi belgini kiriting')
+
+
+console.info(belgi)
+for (let i = 1; i< zina ; i++){
+    console.info (belgi = qator+belgi)
+}
